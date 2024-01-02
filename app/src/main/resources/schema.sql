1aa46ef5796928fd694cbaf048c3b3f4db8c7574
Alek Przybylo <aleksander.przybylo@polymtl.ca>
2024-01-02 09:00:38 -0800

    sql script updates

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,3 +1,12 @@
+CREATE DATABASE yoga
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
 CREATE TABLE public.articles
 (
     text text COLLATE pg_catalog."default" NOT NULL,
